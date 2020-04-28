73e01b5f9def89f39b1bd050b653451d36ca00a5
Joey Bruno <brunojoey12@gmail.com>
2020-04-28 12:55:35 -0500

    Reconfigured TinyMCE component.

--- client/src/components/TinyMCE/index.js
+++ client/src/components/TinyMCE/index.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { Editor } from '@tinymce/tinymce-react';
 
-function TinyMCE () {
-    return (
-        TinyMCE.init({
-            selector: "#textArea",
-            plugins: "codesample",
-            menu: {
-                file: { title: 'File', items: 'codesameple | newdocument restoredraft | preview | print ' },
-                edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall | searchreplace' },
-                view: { title: 'View', items: 'code | visualaid visualchars visualblocks | spellchecker | preview fullscreen' },
-                insert: { title: 'Insert', items: 'image link media template codesample inserttable | charmap emoticons hr | pagebreak nonbreaking anchor toc | insertdatetime' },
-                format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align | forecolor backcolor | removeformat' },
-                tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | code wordcount' },
-                table: { title: 'Table', items: 'inserttable | cell row column | tableprops deletetable' },
-                help: { title: 'Help', items: 'help' }
-              },
-              width: 600,
-              height: 600
-        })
-    )
-};
-
+class TinyMCE extends React.Component {
+    handleEditorChange = (content, editor) => {
+      console.log('Content was updated:', content);
+    }
+ 
+    render() {
+      return (
+        <Editor
+          initialValue="<p>This is the initial content of the editor</p>"
+          init={{
+            height: 500,
+            width: 600,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar:
+              'undo redo | formatselect | bold italic backcolor | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent | removeformat | help'
+          }}
+          onEditorChange={this.handleEditorChange}
+        />
+      );
+    }
+  }
+ 
 export default TinyMCE;
