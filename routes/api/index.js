dc1d1f8070985db1cff420bfae4583c88e49e018
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-28 11:26:41 -0500

    move login and logout routes to root routes folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,21 +7,4 @@ const snipRoutes = require('./snips');
 router.use('/users', userRoutes);
 router.use('/snips', snipRoutes);
 
-// Route to login. Uses passport.authenticate middleware that was set up with local strategy.
-// If the user has valid login credentials, sign them in. Otherwise send an error.
-router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }, (req, res) => {
-  if (req.user) { return res.json(req.user); }
-
-  // Login was unsuccessful.
-  return res.json({ 'error': 'no user' });
-}));
-
-// Route to terminate a login session. According to passport docs, invoking req.logout() will
-// remove the req.user property and clear the lgoin session (if any).
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login');
-});
-
-
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,23 @@ const apiRoutes = require('./api');
 // Catch all API routes.
 router.use('/api', apiRoutes);
 
-// If no API routes are hit, send the React app.
+// Route to login. Uses passport.authenticate middleware that was set up with local strategy.
+// If the user has valid login credentials, sign them in. Otherwise send an error.
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }, (req, res) => {
+  if (req.user) { return res.json(req.user); }
+
+  // Login was unsuccessful.
+  return res.json({ 'error': 'no user' });
+}));
+
+// Route to terminate a login session. According to passport docs, invoking req.logout() will
+// remove the req.user property and clear the lgoin session (if any).
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+// If no other routes are hit, send the React app.
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
