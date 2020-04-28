ebd5d401ad7588dea6335eb913d4b2bbf51ba75a
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-27 21:33:58 -0500

    create api controllers for each model

---
+++ controllers/snipsController.js
@@ -0,0 +1,37 @@
+const db = require('../models');
+
+// Methods for snips controller.
+module.exports = {
+  findAll: function(req, res) {
+    db.Snip
+      .find(req.query)
+      .sort({ createdAt: 1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Snip
+      .find(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Snip
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Snip
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Snip
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
