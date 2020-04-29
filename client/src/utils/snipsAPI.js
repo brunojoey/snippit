6048344750d3d8971e0108c6df92878a2eb5550c
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-29 14:23:09 -0500

    create front-end api for snip CRUD operations

---
+++ client/src/utils/snipsAPI.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export default {
+  // Create snip.
+  createSnip: function(data) {
+    return axios.post('/api/snips/', data);
+  },
+  // Get all snips.
+  getSnips: function() {
+    return axios.get('/api/snips');
+  },
+  // Get snip with given id.
+  getSnip: function(id) {
+    return axios.get('/api/snips/' + id);
+  },
+  // Update snip with data.
+  updateSnip: function(id, data) {
+    return axios.put('/api/snips/' + id, data);
+  },
+  // Delete snip with given id.
+  deleteSnip: function(id) {
+    return axios.delete('/api/snips/' + id);
+  }
+};
