121757b03bc55435c25bc7f2877b2fce4d0e5f83
antihero <mrcansler@gmail.com>
2020-04-30 04:39:32 -0500

    pages v2

---
+++ client/src/pages/profilePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Feed, ProfilePanel, NavDrop } from "../components";
+
+function ProfilePage() {
+    return (
+        <div>
+            <NavDrop />
+            <ProfilePanel />
+            <div className="row">
+                <Feed />
+            </div>
+        </div>
+    );
+}
