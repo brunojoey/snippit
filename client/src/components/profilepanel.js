c07fada4719fd6fa224f9a778fe998b88b3e4c14
antihero <mrcansler@gmail.com>
2020-04-30 04:22:38 -0500

    panel wireframe

--- client/src/components/profilepanel.js
+++ client/src/components/profilepanel.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { } from "react-materialize";
 
 function ProfilePanel() {
     return (
-        <div>
-            
+        <div className="row">
+            <div className="col s4">
+                <img href={this.user.image}></img>
+            </div>
+            <div className="col s8">
+                
+            </div>
         </div>
     );
 }
