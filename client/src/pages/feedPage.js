ca2f31b0b6085ed8b878da0617dd23693d0164e0
antihero <mrcansler@gmail.com>
2020-04-30 04:43:43 -0500

    feed page

---
+++ client/src/pages/feedPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavDrop, Feed, TinyMCE } from "../components";
+
+function FeedPage() {
+    return (
+        <div>
+            <NavDrop />
+            <div className="row">
+                <form method="post">
+                    <TinyMCE />
+                </form>
+            </div>
+            <div className="row">
+                <Feed />
+            </div>
+        </div>
+    );
+}
