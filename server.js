850bce781328d0ce856cd1fdc99dbe90e01efd7d
Joey Bruno <brunojoey12@gmail.com>
2020-04-30 13:09:30 -0500

    Added route const to server again.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require('express-session');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
