d0a697800babdb28b766e07559f1926a4b175fa8
antihero <mrcansler@gmail.com>
2020-04-30 20:49:42 -0500

    added editor theme

--- client/src/components/AceEditor/index.js
+++ client/src/components/AceEditor/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AceEditor from "react-ace";
 import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 function SnipEditor() {
     function onChange(newValue) {
