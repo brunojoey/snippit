730c71c3279c8f57774de99530b4d6c0f5904338
Tyler Kennedy <tkennedy118@gmail.com>
2020-05-25 10:26:16 -0500

    update navbar button styles

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Icon, NavItem, Button } from 'react-materialize';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Nav(props) {
   const path = props.path;
@@ -10,32 +12,32 @@ function Nav(props) {
       alignLinks="right"
       brand={<a className="brand-logo" href="/home"><span>{`</`}</span>Snippit<span>></span></a>}
       centerLogo
-      id="mobile-nav"
-      menuIcon={<Icon>menu</Icon>}
+      // id="mobile-nav"
+      menuIcon={<FontAwesomeIcon className='v-align' size='2x' icon={faBars}></FontAwesomeIcon>}
       options={{
         draggable: true,
-        edge: 'right',
-        inDuration: 250,
+        edge: 'left',
+        inDuration: 100,
+        outDuration: 100,
         onCloseEnd: null,
         onCloseStart: null,
         onOpenEnd: null,
         onOpenStart: null,
-        outDuration: 200,
         preventScrolling: true
       }}
     >
       {(path !== '/login' && path !== '/signup') 
         ?
-          <Link to='/login' type='button' className='btn-rounded red-btn' onClick={() => props.setPath('/login')} style={{ width: '108px' }}>
-            <div style={{ textAlign: 'center', height: '36px', marginTop: '-16px' }}>$&#123;&#32;Login&#32;&#125;</div>
+          <Link to='/login' type='button' onClick={() => props.setPath('/login')} className='red-btn nav-btn'>
+            <div className='nav-btn-text'>$&#123;&#32;Login&#32;&#125;</div>
           </Link> 
         : 
           <></>
         }
       {(path !== '/login' && path !== '/signup')
         ? 
-          <Link to='/signup' type='button' onClick={() => props.setPath('/signup')} className='btn-rounded red-btn' style={{ width: '108px' }}>
-            <div style={{ textAlign: 'center', height: '36px', marginTop: '-16px' }}>$&#123;&#32;Signup&#32;&#125;</div>
+          <Link to='/signup' type='button' onClick={() => props.setPath('/signup')} className='red-btn nav-btn'>
+            <div className='nav-btn-text'>$&#123;&#32;Signup&#32;&#125;</div>
           </Link> 
         : 
           <></>
