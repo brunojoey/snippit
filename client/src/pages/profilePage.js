bd3bd9884cebdb4bb782225a00889f3dbfddb43b
Joey Bruno <brunojoey12@gmail.com>
2020-06-06 10:10:07 -0500

    Got profile image functionality to work.

--- client/src/pages/profilePage.js
+++ client/src/pages/profilePage.js
@@ -1,60 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useContext, useState } from 'react';
-import ProfilePanel from '../components/Profile/index.js';
-import Foot from '../components/Footer';
-import { Row } from 'react-materialize';
-import usersAPI from '../utils/usersAPI';
-import StatusContext from '../utils/StatusContext';
-
-function Profile() {
-  const { status } = useContext(StatusContext);
-
-  const [biography, setBiography] = useState();
-  const [github, setGithub] = useState();
-  const [linkedin, setLinkedin] = useState();
-
-  useEffect(() => {
-    console.log("STATUS", status);
-    async function fetchData() {
-        const { data } = await usersAPI.getUser(status._id);
-        console.log("USER ID", data);
-        if (data.linkedin) { setLinkedin(data.linkedin); }
-        if (data.github) { setGithub(data.github); }
-        // if (data.biography) { setBiography(data.biography); }
-    }
-    fetchData();
-//     getUser();
-//     console.log("USER", user);
-//     // Used to handle form submission.
-//     async function fetchData() {      
-//           // Create new response snip and add it to the main snip's responses array.
-//           await usersAPI.updateGithub(githubId, { newGithub: data._id } );
-//           await usersAPI.updateLinkedIn(linkedInId, { newLinkedin: data._id } );
-    
-
-// //         // reference
-//         //   github = newGithub;
-//         //   linkedIn = newLinkedin;
-//         //   biography = newBio;
-  
-//           const { data } = await usersAPI.createBio(biography);
-//           console.log("BIO", biography = newBio);
-//         }
-
-//         fetchData();
-}, [github, linkedin, biography] );
-
-  return (
-    <>
-      <div className="containter home-container">
-        <Row>
-          <ProfilePanel linkedin={linkedin} github={github} biography={biography} setBiography={setBiography} setLinkedin={setLinkedin} setGithub={setGithub} status={status} />
-        </Row>
-      </div>
-      <>
-        <Foot />
-      </>
-=======
 import React, { useEffect, useState } from 'react';
 import ProfilePanel from '../components/Profile/index.js';
 import { Container, Row } from 'react-materialize';
@@ -94,7 +37,6 @@ function Profile(props) {
       <Container>
         <ProfilePanel state={state} setState={setState} />
       </Container>
->>>>>>> 822366cf2d09d02059eeb54e22add3da80122a70
     </>
   );
 };
